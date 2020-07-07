e54645a00177e7159e9751b884beadbddc26594e
Simon Dell <simon.dell@founders4schools.org.uk>
2020-07-07 16:44:37 +0100

    Read new art from the API
    Move the actual reading out to a separate function. This mimics how you might have an API module. Add variables for tracking page and page count. Add function to request the next page; add function to capture results of read request.
    
    All this works, but somehow I factored out useEffect. This seems counter to the intent of hooks and where FB wants us to take functional components.

--- src/Gallery/Gallery.tsx
+++ src/Gallery/Gallery.tsx
@@ -31,27 +31,43 @@ export interface Record {
   title: string;
 }
 
+async function readRecords (
+  page: number,
+  resolve: Function,
+){
+  try {
+    const url = `https://api.harvardartmuseums.org/object?classification=Prints&hasimage=1&sort=title&sortorder=desc&apikey=c28e4be0-4c0e-11ea-90d6-25d9a9fe80fc&size=10&page=${page}`;
+    const response = await fetch(url);
+
+    if(!response.ok) throw new Error('Error reading from API');
+
+    resolve(response)
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+
 export default function () {
+  const [page, setPage] = useState(0)
+  const [pageCount, setPageCount] = useState(0)
   const [records, setRecords] = useState([] as Record[]);
 
-  useEffect(() => {
-    async function readRecords () {
-      try {
-        const url = 'https://api.harvardartmuseums.org/object?classification=Prints&hasimage=1&sort=title&sortorder=desc&apikey=c28e4be0-4c0e-11ea-90d6-25d9a9fe80fc&size=10&page=2';
-        const response = await fetch(url);
+  async function captureRecords (response: Response) {
+    const body = await response.json()
+    setPage(body.info.page)
+    setPageCount(body.info.pageCount)
+    setRecords([
+      ...records,
+      ...body.records,
+    ])
+  }
 
-        if(!response.ok) throw new Error('Error reading records from API');
+  function readNextPage () {
+    if(pageCount && page && page >= pageCount) return;
 
-        const body = await response.json()
-        setRecords(body.records)
-      }
-      catch (err) {
-        console.error(err)
-      }
-    }
-
-    readRecords()
-  }, [])
+    readRecords(page + 1, captureRecords);
+  }
 
   return (
     <>
@@ -66,7 +82,7 @@ export default function () {
     }
     </GalleryGrid>
     <Waypoint
-      onEnter={() => { console.log('enter!'); }}
+      onEnter={readNextPage}
       topOffset="-100px"
     >
       <ProgressWrapper>
